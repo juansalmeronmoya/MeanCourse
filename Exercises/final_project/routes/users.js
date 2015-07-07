da5baba44546a416fdd5886837c72dd0d8f287ed
Juan Salmerón Moya <jnssterrass@gmail.com>
2015-07-07 18:08:06 +0200

    blank routers

--- Exercises/final_project/routes/users.js
+++ Exercises/final_project/routes/users.js
@@ -5,20 +5,11 @@ var Contact = require('mongoose').model('Contact');
 var ObjectId = require('mongoose').Types.ObjectId;
 
 router.get('/', function(req, res, next){
-  People.find({}, {'_id':false}, function(err, data) {
-    if(!err) {
-      res.status(200).json(data);
-    }
-  })
+
 });
 
 router.get('/:id', function (req, res, next) {
-  var peopleId = req.params.id;
-  People.find({_id: new ObjectId(peopleId)}, {_id: false}, function(err, data) {
-    if(!err) {
-      res.status(200).json(data);
-    }
-  })
+
 });
 
 router.post('/', function(req, res, next) {
@@ -26,12 +17,7 @@ router.post('/', function(req, res, next) {
 });
 
 router.delete('/:id', function(req, res, next) {
-  var taskId = req.params.id;
-  Ship.remove({_id: new ObjectId(taskId)}, function(err){
-    if(!err) {
-      res.status(200).send(true);
-    }
-  });
+
 });
 
 router.patch('/:id', function(req, res, next) {
