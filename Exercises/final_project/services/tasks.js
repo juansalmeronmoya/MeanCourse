140be669a5acaef6215860106e509497bccf2dce
Juan Salmerón Moya <jnssterrass@gmail.com>
2015-07-08 15:34:24 +0200

    authentication

--- Exercises/final_project/services/tasks.js
+++ Exercises/final_project/services/tasks.js
@@ -1,18 +1,46 @@
 angular.module('services').factory('tasksService', tasksService);
 
-function tasksService() {
-  var taskslist = [];
+function tasksService($http, $q, AuthService) {
+  var SERVER_URL = 'http://localhost:8080/';
 
-  var addTask = function(task) {
-    taskslist.push(task);
-  };
+  function getTasks() {
+    var q = $q.defer();
 
-  var removeTask = function($index) {
-    taskslist.splice($index, 1);
-  };
+    if(AuthService.user) {
+      console.log(AuthService.getUser());
+      $http.get(SERVER_URL + 'user/' + AuthService.getUser().user._id + '/task').then(function(data){
+        q.resolve(data);
+      }, function(){
+        q.reject();
+      });
+    } else {
+      q.reject();
+    }
+    return q.promise;
+  }
+
+  function addTask(newTask) {
+    var q = $q.defer();
+    $http.post(SERVER_URL, newTask).then(function(data){
+      q.resolve(data)
+    }, function(){
+      q.reject(data);
+    });
+    return q.promise;
+  }
+
+  function removeTask(id) {
+    var q = $q.defer();
+    $http.delete(SERVER_URL + id).then(function(data){
+      q.resolve(data);
+    }, function(data) {
+      q.reject(data);
+    });
+    return q.promise;
+  }
 
   return {
-    tasks: taskslist,
+    getTasks: getTasks,
     addTask: addTask,
     removeTask: removeTask
   }
